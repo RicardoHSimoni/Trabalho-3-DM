e20bb82d68fb3156a5615dcfa19678af803392df
Joaovpnovaes <joaovictorpiresnovaes@gmail.com>
2025-05-07 10:55:31 -0300

    estrutura do app
    sem muitas coisas, só o basico mesmo.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/splash/animated_splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Atividade 02',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AnimatedSplashScreen(),
+    );
+  }
+}
